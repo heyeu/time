c4dd023db6f7d875645b2f55ea22f8b233c5ffc2
WonJik Yang <wonjikyang@gmail.com>
2018-03-15 20:44:03 +0100

    each timezone corresponds to its converted hex color

--- js/main.js
+++ js/main.js
@@ -1,46 +1,61 @@
 let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
 let grid = [];
+let hours = [];
 let colors = [];
-let ttz = 25;
+let newH = 0;
+let gridID = 0;
+let colorID = 0;
+let color;
+let totalTimeZones = 25;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let w = window.innerWidth/25;
 
-function createTimeZone(color){
-  for (let i = 0; i < ttz; i++){
-    ctx.fillStyle = color;
+function displayTimeZone(colors){
+  for (let i = 0; i < totalTimeZones; i++){
+    ctx.fillStyle = colors[i];
     ctx.fillRect((i * w) ,0, w-5, canvas.height);
-    // grid.push(ttz[i]);
-    // console.log(grid[5]);
   }
 }
 
-function refreshData() {
+function assignTimeZone(colors){
+  for (let i = 0; i < colors.length; i++){
+    console.log(colors[i]);
+    displayTimeZone(colors);
+  }
 
+}
+
+function fetchColor() {
+
+  //converting time value to colors
  	let d = new Date()
  	let h = d.getHours();
  	let m = d.getMinutes();
  	let s = d.getSeconds();
 
- 	if (h<=9) {h = '0'+h};
- 	if (m<=9) {m = '0'+m};
-	if (s<=9) {s = '0'+s};
 
-  for(let i = 0; i < ttz; i++){
 
-    colors.push(ttz[i]);
-    console.log(colors[5]);
+  for (let i = -12; i < 12; i++){
+    let newH = (h+i)*3;
+    if (newH > 24) {
+      newH = newH - 24 ;
+    };
+    if (newH<=9) {newH = '0'+newH};
+    if (m<=9) {m = '0'+m};
+    if (s<=9) {s = '0'+s};
+    color = '#' + newH + m + s;
+    colors.push(color);
   }
- 	let	color = '#'+h+m+s;
 
-  createTimeZone(color);
+  // console.log(colors.splice(0,24));
+  assignTimeZone(colors.splice(0,24));
+  // return;
 
-    // $("div.background").css("background-color", color );
-    // $("p#hex").text(color);
-
-    setTimeout(refreshData, 1000);
+  setTimeout(fetchColor, 1000);
 }
 
-refreshData(); // execute function
+// fetchColorId();
+fetchColor(); // execute function
